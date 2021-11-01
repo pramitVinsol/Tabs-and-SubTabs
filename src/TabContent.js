62bd2b39dcb3daf3c19bff108fa3cf082ee4db74
Ubuntu <pramit@vinsol.com>
2021-11-01 11:22:36 +0530

    indentation

--- src/TabContent.js
+++ src/TabContent.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 function TabContent(props) {
-    return(
-      <div>
-        <h3>{props.heading}</h3>
-        <p>{props.content}</p>
-      </div>
-    )
+  return(
+    <div>
+      <h3>{props.heading}</h3>
+      <p>{props.content}</p>
+    </div>
+  )
 }
 
 export default TabContent;
